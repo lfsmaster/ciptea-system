f395fc7f57c7e385d0cdf1e0f56af84230c98da6
lfsmaster <147679840+lfsmaster@users.noreply.github.com>
2026-07-21 15:20:57 -0400

    chore: ajustar verificação do estado interno de OCR

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,7 @@ export default tseslint.config(
     plugins: { 'react-hooks': reactHooks, 'react-refresh': reactRefresh },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^detecting$', argsIgnorePattern: '^_' }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
     }
   }
